04541c572b0d5c9ac561addac30b5b8775acc016
mlauter <lauter.miriam@gmail.com>
2014-09-17 17:57:42 -0400

    first pass plotting usgs earthquake data with canada icons

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+import os
+
+app = Flask(__name__, static_url_path='')
+
+@app.route('/', methods=['GET'])
+@app.route('/hello_maps', methods=['GET'])
+def homepage():
+    return render_template('hello_maps.html', key=os.environ["GOOGLE_MAPS_API_KEY"])
+
+if __name__ == "__main__":
+    app.run(debug=True)
